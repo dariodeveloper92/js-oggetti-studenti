fdf3a8ee9980511dbef10b6fca0068ced81a65b4
Dario <dario.defrancisci92@gmail.com>
2021-07-30 17:08:46 +0200

    update esercizio

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ let datiStudente = [
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 // Accediamo a tutte le proprietà di un oggetto //
 for (let key in datiStudente) {
-    //console.log(datiStudente[key]);
+    console.log(datiStudente[key]);
 }
 
 //Creare un array di oggetti di studenti.
@@ -22,19 +22,34 @@ let studenti = [
         strudente1 : 'Filippo',
     },
     {
-        studente2: 'Paolo',
+        studente2: 'Alessio',
     },
     {
-        studente3: 'Alessio'
+        studente3: 'Paolo'
     }
 ]
 
 //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-studenti.push(datiStudente);
-//console.log(studenti);
-
-studenti.dati = 'nome: ' + 'cognome: '; 
+//studenti.push( 'nome: ' + 'cognome: ')
+studenti.nome = ' ';
+studenti.cognome = ' ';
 console.log(studenti);
 
-//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+//studenti.dati =  'nome: ' + 'cognome: ';
+//console.log(studenti);
 
+//Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+//VARIABILI - NUMERI INSERITI DALL'UTENTE
+let oggettiUtente = [];
+
+for (i =0; i < 3; i++) { //Se devo creare "3 PROMPT" utilizzo il Ciclo "For"
+    //oggettiUtente.push(prompt('Inserisci in ordine: nome, cognome, e età'));//Per "INSERIRE" all'interno dell'Array un nuovo oggetto studente utilizzo il METODO "PUSH"
+    //studenti.push(oggettiUtente);
+    
+    studenti.oggettiUtente = (prompt('Inserisci in ordine: nome, cognome, e età'));;
+    //console.log(studenti);
+}
+
+for (let chiave in studenti) {
+    console.log(chiave + ' ' + oggettiUtente[chiave]);
+}
