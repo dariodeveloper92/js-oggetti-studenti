bd98e9462b87b9364ed4877d5ea9714d154d5a98
Dario <dario.defrancisci92@gmail.com>
2021-07-30 18:00:32 +0200

    Esercizio Terminato

--- js/main.js
+++ js/main.js
@@ -19,37 +19,48 @@ for (let key in datiStudente) {
 //Creare un array di oggetti di studenti.
 let studenti = [
     {
-        strudente1 : 'Filippo',
+        nome : 'Filippo ',
+        cognome : 'Rossi ',
+        eta : 29
     },
     {
-        studente2: 'Alessio',
+        nome : 'Alessio ',
+        cognome : 'Biachi ',
+        eta : 22
     },
     {
-        studente3: 'Paolo'
+        nome : 'Paolo ',
+        cognome : 'Verdi ',
+        eta : 32
     }
 ]
 
 //Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-//studenti.push( 'nome: ' + 'cognome: ')
-studenti.nome = ' ';
-studenti.cognome = ' ';
-console.log(studenti);
+for (i = 0; i < studenti.length; i++)  { //ESEGUO UN CICLO FOR E ISNERISCO NELL'ARRAY STUDENTI: NOME E COGNOME
+    console.log(studenti[i]['nome']+ studenti[i]['cognome']);
+    //console.log(studenti[i].nome + studenti[i].cognome);
+}
 
-//studenti.dati =  'nome: ' + 'cognome: ';
-//console.log(studenti);
 
 //Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 //VARIABILI - NUMERI INSERITI DALL'UTENTE
 let oggettiUtente = [];
+var numPrompt = 3;
 
-for (i = 0; i < 3; i++) { //Se devo creare "3 PROMPT" utilizzo il Ciclo "For"
-    //oggettiUtente.push(prompt('Inserisci in ordine: nome, cognome, e età'));//Per "INSERIRE" all'interno dell'Array un nuovo oggetto studente utilizzo il METODO "PUSH"
-    //studenti.push(oggettiUtente);
-    
-    studenti.oggettiUtente = (prompt('Inserisci in ordine: nome, cognome, e età'));;
-    //console.log(studenti);
+for (i = 0; i < numPrompt; i++) { //Se devo creare "3 PROMPT" utilizzo il Ciclo "For"
+    oggettiUtente.push(prompt('Inserisci in ordine: nome, cognome, e età'));//Per "INSERIRE" all'interno dell'Array un nuovo oggetto studente utilizzo il METODO "PUSH"
 }
 
-for (let chiave in studenti) {
-    console.log(chiave + ' ' + oggettiUtente[chiave]);
+console.log(oggettiUtente); //STAMPO SU CONSOLE oggettiUtente
+
+//Faccio un OBJECTS (nuovoStudente) e richiamo il primo / secondo / terzo "Array" 
+let nuovoStudente = {
+    nome : oggettiUtente[0], //RICHIAMO IL PRIMO ARRAY
+    cognome : oggettiUtente[1],//RICHIAMO IL SECONDO ARRAY
+    eta : oggettiUtente[2]//RICHIAMO IL TERZO ARRAY
 }
+
+console.log(nuovoStudente);//STAMPO SU CONSOLE nuovoStudente
+
+studenti.push(nuovoStudente); //INSERISCO "nuovoStudente" ALL'INERNO DELL'ARRAY OGGETTI "studenti" con il METODO "PUSH"
+console.log(studenti);//STAMPO SU CONSOLE studenti
